a9eedf4dd63a7033dce6ad121c5611a812e31427
Ronan-O-Murchu <ronan.murphy.design@gmail.com>
2021-07-25 22:21:29 +1200

    java docs:
    tidy up code and include event key functions to allow the user to press enter instead of clicking submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,12 +14,23 @@ document.addEventListener("DOMContentLoaded", function() {
 			}
         });
     }
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+    	if (event.key === "Enter") {
+    		checkAnswer();
+    	}
+    })
+
     runGame("addition");
 });
 
 /** This is the first loop to run when the webpage is loaded */
 
 function runGame (gameType) {
+
+	document.getElementById("answer-box").value = "";
+	document.getElementById("answer-box").focus();
+
 	/** This code creates 2 random numbers between 1 and 24 */
 	let num1 = Math.floor(Math.random() * 25) + 1;
 	let num2 = Math.floor(Math.random() * 25) + 1;
