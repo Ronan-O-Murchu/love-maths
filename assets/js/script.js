c39bd87ef35a36d5df49a4552fa5266328fbb2dd
Ronan-O-Murchu <ronan.murphy.design@gmail.com>
2021-07-25 19:28:38 +1200

    java docs:
    create structure to run addition game type and generate random number values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+/** This is the first script that will load when the webpage is loaded 
+ * and it is impotant to have this run otherwise the page will not work
+ * */
+
 document.addEventListener("DOMContentLoaded", function() {
 	let buttons = document.getElementsByTagName("button");
 	for (let button of buttons) {
@@ -6,15 +10,29 @@ document.addEventListener("DOMContentLoaded", function() {
 				alert("You clicked submit!");
 			} else {
 				let gameType = this.getAttribute("data-type");
-				alert(`You clicked ${gameType}`);
+				runGame(gameType);
 			}
         });
     }
+    runGame("addition");
 });
 
-function runGame () {
+/** This is the first loop to run when the webpage is loaded */
+
+function runGame (gameType) {
+	/** This code creates 2 random numbers between 1 and 24 */
 	let num1 = Math.floor(Math.random() * 25) + 1;
 	let num2 = Math.floor(Math.random() * 25) + 1;
+
+/** This is the first loop to run when the webpage is loaded */
+	if (gameType === "addition") {
+		displayAdditionQuestion(num1, num2);
+	} 
+/** If the game type does not laod then the below alert will pop up */
+	else {
+		alert(`Unknown game type: ${gameType}`);
+		throw `Unknown game type: $(gameType). Aborting!`;
+	}
 }
 
 function checkAnswer () {
@@ -33,8 +51,10 @@ function incrementWrongAnswer () {
 
 }
 
-function displayAdditionQuestion () {
-
+function displayAdditionQuestion (operand1, operand2) {
+	document.getElementById('operand1').textContent = operand1;
+	document.getElementById('operand2').textContent = operand2;
+	document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion () {
